0592bd108a3526b6fcb51a421ceb0da0e3958ccc
Marc Martínez Miró <marcm2003g@gmail.com>
2023-10-08 17:34:41 +0200

    V3 finished

--- hash01/Program.cs
+++ hash01/Program.cs
@@ -10,37 +10,78 @@ namespace hash01
         static void Main(string[] args)
         {
             String filePath = null;
-            Console.Write("Enter file path: ");
+            Console.Write("Introdueix la ruta de l'arxiu: ");
             while (string.IsNullOrEmpty(filePath))
             {
                 Console.Clear();
-                Console.Write("Enter file path: ");
+                Console.Write("Introdueix la ruta de l'arxiu: ");
                 filePath = Console.ReadLine();
+            }
+
+            Console.WriteLine("Selecciona una opció:");
+            Console.WriteLine("1. Generar hash");
+            Console.WriteLine("2. Verificar integritat de l'arxiu");
+
+            int opcio = 0;
+            while (opcio != 1 && opcio != 2)
+            {
+                Console.Write("Introdueix l'opció: ");
+                if (!int.TryParse(Console.ReadLine(), out opcio))
+                {
+                    opcio = 0;
+                }
             }
 
             try
             {
-                string fileContents = File.ReadAllText(filePath);
-            // Convertim l'string a un array de bytes
-                byte[] bytesIn = Encoding.UTF8.GetBytes(fileContents);
-            // Instanciar classe per fer hash
+                if (opcio == 1)
+                {
+            
+                    string contingutArxiu = File.ReadAllText(filePath);
 
-            // fent servir using ja es delimita el seu àmbit i no cal fer dispose
-                using (SHA512Managed SHA512 = new SHA512Managed())
-            {
-                // Calcular hash
-                byte[] hashResult = SHA512.ComputeHash(bytesIn);
-                
-                // Si volem mostrar el hash per pantalla o guardar-lo en un arxiu de text
-                // cal convertir-lo a un string
-                    String textOut = BitConverter.ToString(hashResult).Replace("-", string.Empty);
+                    byte[] bytesArxiu = Encoding.UTF8.GetBytes(contingutArxiu);
 
-                    string hashFilePath = Path.ChangeExtension(filePath, ".SHA");
-                    File.WriteAllText(hashFilePath, textOut);
+                    using (SHA512Managed SHA512 = new SHA512Managed())
+                    {
+                        byte[] resultatHash = SHA512.ComputeHash(bytesArxiu);
 
-                    Console.WriteLine("Hash of file {0}", filePath);
-                    Console.WriteLine(textOut);
-                    Console.ReadKey();
+                        
+                        String textOut = BitConverter.ToString(resultatHash).Replace("-", string.Empty);
+
+                        string rutaHash = Path.ChangeExtension(filePath, ".SHA");
+                        File.WriteAllText(rutaHash, textOut);
+
+                        Console.WriteLine("Hash de l'arxiu {0}", filePath);
+                        Console.WriteLine(textOut);
+                        Console.ReadKey();
+                    }
+                }
+                else if (opcio == 2)
+                {
+                    string rutaHash = Path.ChangeExtension(filePath, ".SHA");
+                    string hashEsperat = File.ReadAllText(rutaHash);
+
+                    string contingutArxiu = File.ReadAllText(filePath);
+
+                    byte[] bytesArxiu = Encoding.UTF8.GetBytes(contingutArxiu);
+
+                    using (SHA512Managed SHA512 = new SHA512Managed())
+                    {
+                        byte[] resultatHash = SHA512.ComputeHash(bytesArxiu);
+
+                        String hashActual = BitConverter.ToString(resultatHash).Replace("-", string.Empty);
+
+                        if (hashEsperat == hashActual)
+                        {
+                            Console.WriteLine("L'arxiu {0} no ha estat modificat", filePath);
+                        }
+                        else
+                        {
+                            Console.WriteLine("L'arxiu {0} ha estat modificat", filePath);
+                        }
+
+                        Console.ReadKey();
+                    }
                 }
             }
             catch (Exception ex)
